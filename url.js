882136835376bd65e1ba42d4641330e94351d719
ppz <372301467@qq.com>
2023-05-12 15:51:55 +0800

    fix: query in url.format

--- url.js
+++ url.js
@@ -1,6 +1,6 @@
 exports.format = function({ hostname, pathname, query }) {
   let result = `otpauth://${hostname}/${pathname}?`
   for(const key in query)
-    result += key + '=' + query[key] + '&'
-  return result.slice(0, result.length - 1)
+    result += key + '=' + encodeURIComponent(query[key]) + '&'
+  return result.slice(0, -1)
 }
