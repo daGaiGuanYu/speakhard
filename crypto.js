d8edc05e0aebb1a13ac39d9b15f5d704d720655b
ppz <372301467@qq.com>
2023-12-15 10:59:34 +0800

    fix: rm dep (randombyte)

--- crypto.js
+++ crypto.js
@@ -1,1 +1,5 @@
-exports.randomBytes = require('randombytes')
+exports.randomBytes = function() {
+  const randomBytes = new Uint8Array(8)
+  crypto.getRandomValues(randomBytes)
+  return randomBytes
+}
